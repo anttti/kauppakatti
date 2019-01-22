84341834ba084bce3e1af09e68186cfa1c172b71
Antti Mattila <antti.mattila@futurice.com>
2019-01-22 21:24:50 +0200

    Add support for async await

--- src/index.js
+++ src/index.js
@@ -27,49 +27,42 @@ const isValidPayload = payload => {
   return true;
 };
 
-const getMyLists = uid => {
-  return db
-    .collection("shoppinglists")
-    .where("owners", "array-contains", uid)
-    .get()
-    .then(
-      querySnapshot => {
-        return querySnapshot.docs.map(list => {
-          return {
-            ...list.data(),
-            id: list.id
-          };
-        });
-      },
-      error => {
-        console.error("Error reading fetching my lists:", error);
-        return [];
-      }
-    );
+const getMyLists = async uid => {
+  try {
+    const querySnapshot = await db
+      .collection("shoppinglists")
+      .where("owners", "array-contains", uid)
+      .get();
+    return querySnapshot.docs.map(list => {
+      return {
+        ...list.data(),
+        id: list.id
+      };
+    });
+  } catch (error) {
+    console.error("Error reading fetching my lists:", error);
+    return [];
+  }
 };
 
-const getListItems = listId => {
-  return db
-    .collection(`shoppinglists/${listId}/items`)
-    .get()
-    .then(
-      querySnapshot => {
-        return querySnapshot.docs.map(item => {
-          return {
-            ...item.data(),
-            id: item.id
-          };
-        });
-      },
-      error => {
-        console.error("Error reading shopping list items:", error);
-      }
-    );
+const getListItems = async listId => {
+  try {
+    const querySnapshot = await db
+      .collection(`shoppinglists/${listId}/items`)
+      .get();
+    return querySnapshot.docs.map(item => {
+      return {
+        ...item.data(),
+        id: item.id
+      };
+    });
+  } catch (error) {
+    console.error("Error reading shopping list items:", error);
+    return [];
+  }
 };
 
-const start = uid => {
-  let elm;
-
+const start = async uid => {
   let snapshotUnsubscribe;
 
   const updateItems = querySnapshot => {
@@ -82,78 +75,80 @@ const start = uid => {
     elm.ports.itemsUpdated.send(items);
   };
 
-  getMyLists(uid)
-    .then(lists => {
-      const currentlySelectedListId = lists[0].id;
-      return Promise.all([
-        getListItems(currentlySelectedListId),
-        lists,
-        currentlySelectedListId
-      ]);
-    })
-    .then(([currentListItems, lists, currentlySelectedListId]) => {
-      const initialModel = {
-        items: currentListItems,
-        newItemName: "",
-        lists,
-        currentlySelectedListId: lists[0].id,
-        newListName: ""
-      };
-      console.log("initial model", initialModel);
-      elm = Elm.Main.init({
-        node: document.getElementById("root"),
-        flags: initialModel
-      });
-      snapshotUnsubscribe = db
-        .collection(`shoppinglists/${currentlySelectedListId}/items`)
-        .onSnapshot(updateItems);
-      elm.ports.updateDataStore.subscribe(data => {
-        if (!isValidPayload(data)) {
-          return;
-        }
-        console.log("action:", data);
-        switch (data.action) {
-          case "new-list":
-            db.collection("shoppinglists").add({
-              name: data.name,
-              owners: [uid]
-            });
-            break;
-          case "change-list":
-            getListItems(data.listId).then(items => {
-              elm.ports.itemsUpdated.send(items);
-            });
+  const lists = await getMyLists(uid);
 
-            // Set up a snapshot listener & remove the old one
-            if (snapshotUnsubscribe) {
-              snapshotUnsubscribe();
-            }
-            snapshotUnsubscribe = db
-              .collection(`shoppinglists/${data.listId}/items`)
-              .onSnapshot(updateItems);
-            break;
-          case "create":
-            db.collection(`shoppinglists/${data.listId}/items`).add({
-              name: data.name,
-              isDone: false
-            });
-            break;
-          case "update":
-            db.collection(`shoppinglists/${data.listId}/items`)
-              .doc(data.id)
-              .set({
-                name: data.name,
-                isDone: data.isDone
-              });
-            break;
-          default:
-            break;
+  const currentlySelectedListId = lists[0].id;
+  const currentListItems = await getListItems(currentlySelectedListId);
+
+  const initialModel = {
+    items: currentListItems,
+    newItemName: "",
+    lists,
+    currentlySelectedListId: lists[0].id,
+    newListName: ""
+  };
+
+  console.log("initial model", initialModel);
+  const elm = Elm.Main.init({
+    node: document.getElementById("root"),
+    flags: initialModel
+  });
+
+  snapshotUnsubscribe = db
+    .collection(`shoppinglists/${currentlySelectedListId}/items`)
+    .onSnapshot(updateItems);
+
+  elm.ports.updateDataStore.subscribe(data => {
+    if (!isValidPayload(data)) {
+      return;
+    }
+    console.log("action:", data);
+
+    switch (data.action) {
+      case "new-list":
+        db.collection("shoppinglists").add({
+          name: data.name,
+          owners: [uid]
+        });
+        break;
+
+      case "change-list":
+        getListItems(data.listId).then(items => {
+          elm.ports.itemsUpdated.send(items);
+        });
+        // Set up a snapshot listener & remove the old one
+        if (snapshotUnsubscribe) {
+          snapshotUnsubscribe();
         }
-      });
-    });
+        snapshotUnsubscribe = db
+          .collection(`shoppinglists/${data.listId}/items`)
+          .onSnapshot(updateItems);
+        break;
+
+      case "create":
+        db.collection(`shoppinglists/${data.listId}/items`).add({
+          name: data.name,
+          isDone: false
+        });
+        break;
+
+      case "update":
+        db.collection(`shoppinglists/${data.listId}/items`)
+          .doc(data.id)
+          .set({
+            name: data.name,
+            isDone: data.isDone
+          });
+        break;
+
+      default:
+        break;
+    }
+  });
 
   snapshotUnsubscribe = db
     .collection(`shoppinglists`)
+    .where("owners", "array-contains", uid)
     .onSnapshot(querySnapshot => {
       console.log("Liists updated");
     });
